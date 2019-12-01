bc178677950a0ffa083a06188c1bd1383023af72
Franklin <franklintimoteo@live.com>
2019-11-30 22:33:34 -0300

    Auto search templates with glob function. Example spam_android. Create spam using templates found on template directory.

--- app/main/views.py
+++ app/main/views.py
@@ -1,10 +1,19 @@
-from flask import render_template
+import os
+import glob
+from flask import render_template, request, current_app
 from . import main
 
 @main.route('/')
 def index():
     return render_template('index.html')
 
-@main.route('/create_spam')
+
+@main.route('/create_spam', methods=['GET', 'POST'])
 def create_spam():
-    return render_template('create_spam.html')
+    if request.method in "POST":
+        print(request.values)
+        pass
+    template_folder = os.path.join(current_app.root_path, current_app.template_folder)
+    template_glob = os.path.join(template_folder, "spam_*.html")
+    templates = glob.glob(template_glob)
+    return render_template('create_spam.html', templates=templates)
